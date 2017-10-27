12529ad586a5090006455cd6c75bfc3e6079fd33
sgorawski <sgorawski@gmail.com>
2017-10-27 17:54:55 +0200

    Began working on payments

---
+++ hopeItApp/user-modules/src/user/PaymentDetails.java
@@ -0,0 +1,11 @@
+package user;
+
+/**
+ * Created by sgorawski on 27.10.17.
+ */
+public class PaymentDetails {
+    private String notifyUrl;
+    private String customerIP;
+    private String merchantPosID;
+    
+}
