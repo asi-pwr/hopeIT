eccffb8cfede2ed897a783b505d4bd33a585f114
Jakub Dorda <jak_dor@wp.pl>
2017-10-28 09:45:25 +0200

    add notification after new message detected

--- hopeItApp/hopeItApp/app/src/main/java/com/asi/hopeitapp/Messages/MessagesFragment.java
+++ hopeItApp/hopeItApp/app/src/main/java/com/asi/hopeitapp/Messages/MessagesFragment.java
@@ -1,9 +1,15 @@
 package com.asi.hopeitapp.Messages;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,19 +17,24 @@ import android.view.ViewGroup;
 import com.asi.hopeitapp.Main.BaseFragment;
 import com.asi.hopeitapp.Model.Message;
 import com.asi.hopeitapp.Model.Patient;
-import com.asi.hopeitapp.Model.Payment;
 import com.asi.hopeitapp.R;
 import com.bumptech.glide.Glide;
 
+import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static android.app.Notification.DEFAULT_LIGHTS;
+import static android.app.Notification.DEFAULT_VIBRATE;
+
 public class MessagesFragment extends BaseFragment {
 
     @BindView(R.id.messagesRecyclerView)
     RecyclerView recyclerView;
+
+    private int lastMessageSize = 0;
 
     @Nullable
     @Override
@@ -61,7 +72,39 @@ public class MessagesFragment extends BaseFragment {
             return;
         }
 
+        Collections.reverse(messages);
+
         MessagesAdapter adapter = new MessagesAdapter(messages, Glide.with(this), getContext());
         recyclerView.setAdapter(adapter);
+
+        checkIfGotNew(messages);
+    }
+
+    private boolean checkIfGotNew(List<Message> messages){
+
+        SharedPreferences settings = getContext().getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        lastMessageSize = settings.getInt("MessageCount", 0);
+
+        if (lastMessageSize != messages.size()){
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getContext(), "default")
+                    .setDefaults(DEFAULT_VIBRATE | DEFAULT_LIGHTS)
+                    .setSmallIcon(R.drawable.ic_heart)
+                    .setLargeIcon(BitmapFactory.decodeResource(getContext().getResources(),
+                            R.mipmap.ic_launcher))
+                    .setContentTitle("Nowa wiadomość od hopeIt")
+                    .setContentText(messages.get(0).getTitle())
+                    .setAutoCancel(true);
+
+            NotificationManager mNotificationManager = (NotificationManager) getContext()
+                    .getSystemService(Context.NOTIFICATION_SERVICE);
+
+            mNotificationManager.notify(0, mBuilder.build());
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("MessageCount", messages.size());
+            editor.apply();
+        }
+
+        return false;
     }
 }
